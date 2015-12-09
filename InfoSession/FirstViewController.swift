4fba48552d29d965661b51b5154d2aad6b691d77
Seun Makinde <seun@tulip.io>
2015-12-09 10:38:47 -0500

    Initial Commit

---
+++ InfoSession/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InfoSession
+//
+//  Created by Seun Makinde on 2015-12-09.
+//  Copyright © 2015 Seun Makinde. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
